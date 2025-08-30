8770a4a453a4719a6b297a8ea5e9f72923e98d2c
Lisu-Liso <3473622562@qq.com>
2025-08-30 20:36:59 +0800

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "Be sure to run this script with sudo (e.g., 'sudo ./install.sh') to ensure proper permissions."
 mkdir /lib/Coly
-cp Coly /lib/Coly
+cp ./build/Coly /lib/Coly
 chmod +x /lib/Coly
 cp ./Settings/LanguageMap.json /lib/Coly/Settings/LanguageMap.json
 cp ./InteractiveColy.cly /lib/Coly/InteractiveColy.cly
